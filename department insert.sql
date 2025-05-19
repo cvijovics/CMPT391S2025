c04655661b6fcce3b94d858168d0bc478115703d
Stefan Cvijovic <164532223+cvijovics@users.noreply.github.com>
2025-05-19 00:45:04 -0600

    Add files via upload

---
+++ department insert.sql
@@ -0,0 +1,9 @@
+
+USE DummyDB;
+GO
+
+insert into department (department_id, department_name) values (1, 'Art');
+insert into department (department_id, department_name) values (2, 'Science');
+insert into department (department_id, department_name) values (3, 'Music');
+insert into department (department_id, department_name) values (4, 'Engineering');
+insert into department (department_id, department_name) values (5, 'Business');
---
+++ table creation script.sql
@@ -0,0 +1,64 @@
+--Checks if the database exists, if not, creates it
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'DummyDB')
+BEGIN
+    CREATE DATABASE DummyDB;
+END
+GO
+USE DummyDB;
+GO
+
+-- Drop existing tables if they exist
+IF OBJECT_ID('registration', 'U') IS NOT NULL DROP TABLE registration;
+IF OBJECT_ID('course_instance', 'U') IS NOT NULL DROP TABLE course_instance;
+IF OBJECT_ID('course', 'U') IS NOT NULL DROP TABLE course;
+IF OBJECT_ID('student', 'U') IS NOT NULL DROP TABLE student;
+IF OBJECT_ID('instructor', 'U') IS NOT NULL DROP TABLE instructor;
+IF OBJECT_ID('department', 'U') IS NOT NULL DROP TABLE department;
+
+CREATE TABLE department (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(100) NOT NULL,
+)
+
+CREATE TABLE instructor (
+    instructor_id INT PRIMARY KEY,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(department_id)
+)
+
+CREATE TABLE student (
+    student_id INT PRIMARY KEY,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL
+)
+
+CREATE TABLE course (
+    course_id INT PRIMARY KEY,
+    course_name VARCHAR(100) NOT NULL
+)
+
+CREATE TABLE course_instance
+(
+    course_instance_id INT PRIMARY KEY,
+    course_id INT,
+    instructor_id INT,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    days_of_week VARCHAR(3) NOT NULL,
+    max_occupancy INT NOT NULL,
+    current_occupancy INT NOT NULL,
+    FOREIGN KEY (course_id) REFERENCES course(course_id),
+    FOREIGN KEY (instructor_id) REFERENCES instructor(instructor_id)
+)
+
+CREATE TABLE registration (
+    course_instance_id INT,
+    student_id INT,
+    FOREIGN KEY (course_instance_id) REFERENCES course_instance(course_instance_id),
+    FOREIGN KEY (student_id) REFERENCES student(student_id)
+)
+
