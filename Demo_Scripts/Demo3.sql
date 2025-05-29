bd63bc4c6aa4b30fdd014aacfab0ee7f8a9ef737
Stefan Cvijovic <164532223+cvijovics@users.noreply.github.com>
2025-05-29 12:24:39 -0600

    small tweaks to demo script files

--- Demo_Scripts/Demo3.sql
+++ Demo_Scripts/Demo3.sql
@@ -2,6 +2,8 @@ USE CMPT391S2025;
 GO
 
 
-EXEC ConfirmStudentRegistration 
-     9002,  -- Cristiano Ronaldo / student_id
-     2003;    -- CMPT 491 (Advanced) / course_instance_id
+--EXEC ConfirmStudentRegistration 
+  --   9002,  -- Cristiano Ronaldo / student_id
+   --  2003;    -- CMPT 491 (Advanced) / course_instance_id
+
+   --do this in the winforms application
